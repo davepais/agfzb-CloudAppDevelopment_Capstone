2dff202e5a73c4919c6e51744056ae09dcac6ec0
davepais <113855958+davepais@users.noreply.github.com>
2022-11-09 10:55:56 -0500

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["dpaismybluemix.net/static",".localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["dpais.mybluemix.net/static",".localhost", '127.0.0.1']
 
 
 # Application definition
